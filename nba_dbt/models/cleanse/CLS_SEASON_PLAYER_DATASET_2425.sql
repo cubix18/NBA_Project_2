89acbf12a53a3454ff47979bcec3f1b8dc33cb19
cubix199615@wp.pl <cubix199615@wp.pl>
2025-12-02 16:40:42 +0100

    feat: Preparing 2024-25 data

---
+++ nba_dbt/models/cleanse/CLS_SEASON_PLAYER_DATASET_2425.sql
@@ -0,0 +1,122 @@
+{{
+  config(
+    materialized = 'table',
+    tags = ['PLAYER', 'CLEANSE', 'NBA_PROJECT', 'PLAYER_ORIENTED']
+  )
+}}
+
+
+WITH CLS_SEASON_PLAYER_FANTASY_2425 AS (
+    SELECT 
+        *
+    FROM {{ ref('CLS_SEASON_PLAYER_FANTASY_2425') }} 
+),
+
+CLS_SEASON_PLAYER_DATASET_2425 as (
+    SELECT 
+    111111111 AS PLAYER_ID
+    , PLAYER_FULL_NAME
+    , '2024-25' AS PLAYER_SEASON_PLAYED
+    , 2024 AS START_OF_SEASON
+    , 2025 AS END_OF_SEASON
+    , case team_abbreviation
+            when 'ATL' then 'Atlanta'
+            when 'BOS' then 'Boston'
+            when 'BRK' then 'Brooklyn'
+            when 'NJN' then 'New Jersey'
+            when 'CHO' then 'Charlotte'
+            when 'CHH' then 'Charlotte'
+            when 'CHI' then 'Chicago'
+            when 'CLE' then 'Cleveland'
+            when 'DAL' then 'Dallas'
+            when 'DEN' then 'Denver'
+            when 'DET' then 'Detroit'
+            when 'GSW' then 'Golden State'
+            when 'GOS' then 'Golden State'
+            when 'HOU' then 'Houston'
+            when 'SAS' then 'San Antonio'
+            when 'IND' THEN 'Indiana'
+            when 'LAC' then 'Los Angeles'
+            when 'LAL' then 'Los Angeles'
+            when 'MEM' then 'Memphis'
+            when 'MIA' then 'Miami'
+            when 'MIL' then 'Milwaukee'
+            when 'MIN' then 'Minnesota'
+            when 'NOP' then 'New Orleans'
+            when 'NOH' then 'New Orleans'
+            when 'NOK' then 'New Orleans'
+            when 'NYK' then 'New York'
+            when 'NYN' then 'New York'
+            when 'ORL' then 'Orlando'
+            when 'PHI' then 'Philadelphia'
+            when 'PHO' then 'Phoenix'
+            when 'POR' then 'Portland'
+            when 'SAC' then 'Sacramento'
+            when 'SEA' then 'Seattle'
+            when 'TOR' then 'Toronto'
+            when 'UTA' then 'Utah'
+            when 'OKC' then 'Oklahoma City'
+            when 'WAS' then 'Washington'
+            else 'Unknown'
+    end as TEAM_CITY
+    , CASE team_abbreviation
+        when 'ATL' then 'Atlanta Hawks'
+        when 'BOS' then 'Boston Celtics'
+        when 'BRK' then 'Brooklyn Nets'
+        when 'NJN' then 'New Jersey Nets'
+        when 'CHO' then 'Charlotte Hornets'
+        when 'CHH' then 'Charlotte Hornets'
+        when 'CHI' then 'Chicago Bulls'
+        when 'CLE' then 'Cleveland Cavaliers'
+        when 'DAL' then 'Dallas Mavericks'
+        when 'DEN' then 'Denver Nuggets'
+        when 'DET' then 'Detroit Pistons'
+        when 'GSW' then 'Golden State Warriors'
+        when 'GOS' then 'Golden State Warriors'
+        when 'HOU' then 'Houston Rockets'
+        when 'SAS' then 'San Antonio Spurs'
+        WHEN 'IND' THEN 'Indiana Pacers'
+        when 'LAC' then 'Los Angeles Clippers'
+        when 'LAL' then 'Los Angeles Lakers'
+        when 'MEM' then 'Memphis Grizzlies'
+        when 'MIA' then 'Miami Heat'
+        when 'MIL' then 'Milwaukee Bucks'
+        when 'MIN' then 'Minnesota Timberwolves'
+        when 'NOP' then 'New Orleans Pelicans'
+        when 'NOH' then 'New Orleans Hornets'
+        when 'NOK' then 'New Orleans/Oklahoma City Hornets'
+        when 'NYK' then 'New York Knicks'
+        when 'NYN' then 'New York Nets'
+        when 'ORL' then 'Orlando Magic'
+        when 'PHI' then 'Philadelphia 76ers'
+        when 'PHO' then 'Phoenix Suns'
+        when 'POR' then 'Portland Trail Blazers'
+        when 'SAC' then 'Sacramento Kings'
+        when 'SEA' then 'Seattle SuperSonics'
+        when 'TOR' then 'Toronto Raptors'
+        when 'UTA' then 'Utah Jazz'
+        when 'OKC' then 'Oklahoma City Thunder'
+        when 'WAS' then 'Washington Wizards'
+        else 'Unknown'
+    end as TEAM_FULL_NAME
+    , TEAM_ABBREVIATION
+    , PLAYER_AGE
+    , PLAYER_GAMES_PLAYED
+    , 'Unknown' AS PLAYER_GAMES_STARTED
+    , PLAYER_MINUTES_PLAYED
+    , PLAYER_FG_MADE, PLAYER_FG_ATTEMPTED, PLAYER_FG_PCT
+    , PLAYER_3PT_MADE
+    , 'Unknown' AS PLAYER_3PT_ATTEMPTD
+    , 'Unknown' AS PLAYER_3PT_PCT
+    , PLAYER_FT_MADE, PLAYER_FT_ATTEMPTED, PLAYER_FT_PCT
+    , PLAYER_OFF_REB, PLAYER_DEF_REB, PLAYER_TOT_REB
+    , PLAYER_ASSISTS, PLAYER_STEALS, PLAYER_BLOCKS, PLAYER_TOVERS 
+    , NULL AS PLAYER_FOULS
+    , PLAYER_POINTS
+    , 1 AS IS_VALID
+    , TO_TIMESTAMP(CURRENT_TIMESTAMP ) AS LOAD_DATE
+    , 'STAGING_NBA_DATA.CLS_SEASON_PLAYER_FANTASY_2425' AS SOURCE_TABLE
+FROM NBA_DB.CLEANSE_NBA_DATA.CLS_SEASON_PLAYER_FANTASY_2425
+)
+
+SELECT * FROM CLS_SEASON_PLAYER_DATASET_2425
--- nba_dbt/models/cleanse/CLS_SEASON_PLAYER_FANTASY_2425.sql
+++ nba_dbt/models/cleanse/CLS_SEASON_PLAYER_FANTASY_2425.sql
@@ -8,48 +8,51 @@
 WITH FANTASY_TABLE AS (
     
 SELECT
-      TRIM(player)                                  AS player
-    , UPPER(TRIM(team))                             AS team
-    , CAST(age AS INTEGER)                          AS age
-    , CAST(games_played AS INTEGER)                 AS games_played
-    , CAST(minutes AS INTEGER)                      AS minutes
-    , CAST(double_double AS INTEGER)                AS double_double
-    , CAST(triple_double AS INTEGER)                AS triple_double
+      TRIM(player)                                  AS PLAYER_FULL_NAME
+    , UPPER(TRIM(team))                             AS TEAM_ABBREVIATION
+    , CAST(age AS INTEGER)                          AS PLAYER_AGE
+    , CAST(games_played AS INTEGER)                 AS PLAYER_GAMES_PLAYED
+    , CAST(minutes AS INTEGER)                      AS PLAYER_MINUTES_PLAYED
+    , CAST(double_double AS INTEGER)                AS PLAYER_DOUBLE_DOUBLE
+    , CAST(triple_double AS INTEGER)                AS PLAYER_TRIPLE_DOUBLE
 
     -- sums
-    , CAST("9CAT_SUM" AS NUMBER(10,2))              AS cat_9_sum
-    , CAST("11CAT_SUM" AS NUMBER(10,2))             AS cat_11_sum
+    , CAST("9CAT_SUM" AS NUMBER(10,2))              AS PLAYER_CATEGORY_9_LEAGUE
+    , CAST("11CAT_SUM" AS NUMBER(10,2))             AS PLAYER_CATEGORY_11_LEAGUE
 
     -- grades
-    , TRIM(points_grade)                            AS points_grade
-    , TRIM(fg_grade)                                AS fg_grade
-    , TRIM("3PTM_GRADE")                            AS threeptm_grade
-    , TRIM(ft_grade)                                AS ft_grade
-    , TRIM(oreb_grade)                              AS oreb_grade
-    , TRIM(dreb_grade)                              AS dreb_grade
-    , TRIM(treb_grade)                              AS treb_grade
-    , TRIM(ast_grade)                               AS ast_grade
-    , TRIM(stl_grade)                               AS stl_grade
-    , TRIM(blk_grade)                               AS blk_grade
-    , TRIM(to_grade)                                AS to_grade
-    , TRIM(dd_grade)                                AS dd_grade
+    , TRIM(points_grade)                            AS PLAYER_POINTS_GRADE
+    , TRIM(fg_grade)                                AS PLAYER_FG_GRADE
+    , TRIM("3PTM_GRADE")                            AS PLAYER_3PTM_GRADE
+    , TRIM(ft_grade)                                AS PLAYER_FT_GRADE
+    , TRIM(oreb_grade)                              AS PLAYER_OREB_GRADE
+    , TRIM(dreb_grade)                              AS PLAYER_DREB_GRADE
+    , TRIM(treb_grade)                              AS PLAYER_TREB_GRADE
+    , TRIM(ast_grade)                               AS PLAYER_AST_GRADE
+    , TRIM(stl_grade)                               AS PLAYER_STL_GRADE
+    , TRIM(blk_grade)                               AS PLAYER_BLK_GRADE
+    , TRIM(to_grade)                                AS PLAYER_TO_GRADE
+    , TRIM(dd_grade)                                AS PLAYER_DD_GRADE
 
     -- stats
-    , CAST(points AS NUMBER(10,2))                  AS points
-    , CAST(fg_made AS NUMBER(10,2))                 AS fg_made
-    , CAST(fg_attempt AS NUMBER(10,2))              AS fg_attempt
-    , CAST("FG%" AS NUMBER(10,4))                   AS fg_pct
-    , CAST("3PTM" AS NUMBER(10,2))                  AS threeptm
-    , CAST(ft_made AS NUMBER(10,2))                 AS ft_made
-    , CAST(ft_attempt AS NUMBER(10,2))              AS ft_attempt
-    , CAST("FT%" AS NUMBER(10,4))                   AS ft_pct
-    , CAST(oreb AS NUMBER(10,2))                    AS oreb
-    , CAST(dreb AS NUMBER(10,2))                    AS dreb
-    , CAST(treb AS NUMBER(10,2))                    AS treb
-    , CAST(ast AS NUMBER(10,2))                     AS ast
-    , CAST(stl AS NUMBER(10,2))                     AS stl
-    , CAST(blk AS NUMBER(10,2))                     AS blk
-    , CAST("TO" AS NUMBER(10,2))                    AS turnovers
+    , CAST(points AS NUMBER(10,2))                  AS PLAYER_POINTS
+    , CAST(fg_made AS NUMBER(10,2))                 AS PLAYER_FG_MADE
+    , CAST(fg_attempt AS NUMBER(10,2))              AS PLAYER_FG_ATTEMPTED
+    , CAST("FG%" AS NUMBER(10,4))                   AS PLAYER_FG_PCT
+    , CAST("3PTM" AS NUMBER(10,2))                  AS PLAYER_3PT_MADE
+    , CAST(ft_made AS NUMBER(10,2))                 AS PLAYER_FT_MADE
+    , CAST(ft_attempt AS NUMBER(10,2))              AS PLAYER_FT_ATTEMPTED
+    , CAST("FT%" AS NUMBER(10,4))                   AS PLAYER_FT_PCT
+    , CAST(oreb AS NUMBER(10,2))                    AS PLAYER_OFF_REB
+    , CAST(dreb AS NUMBER(10,2))                    AS PLAYER_DEF_REB
+    , CAST(treb AS NUMBER(10,2))                    AS PLAYER_TOT_REB
+    , CAST(ast AS NUMBER(10,2))                     AS PLAYER_ASSISTS
+    , CAST(stl AS NUMBER(10,2))                     AS PLAYER_STEALS
+    , CAST(blk AS NUMBER(10,2))                     AS PLAYER_BLOCKS
+    , CAST("TO" AS NUMBER(10,2))                    AS PLAYER_TOVERS
+    
+    , TO_TIMESTAMP(CURRENT_TIMESTAMP ) AS LOAD_DATE
+    , 'STAGING_NBA_DATA.STG_SEASON_PLAYER_DATASET' AS SOURCE_TABLE
 
 FROM {{ source('STAGING_NBA_DATA', 'STG_SEASON_PLAYER_FANTASY_2425') }}
 
